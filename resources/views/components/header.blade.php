f281e5412fe04aa1d278ff57ff9d989394647a18
P.A.J. Sandaruwan <janith@speralabs.com.au>
2021-10-20 18:01:56 +0530

    Update logo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,7 +3,7 @@
         <div class="d-flex">
             <!-- LOGO -->
             <div class="navbar-brand-box">
-                <a href="index.html" class="logo logo-dark">
+                <a href="{{ route('index') }}" class="logo logo-dark">
                     <span class="logo-sm">
                         <img src="/images/logo.svg" alt="" height="22">
                     </span>
@@ -12,12 +12,12 @@
                     </span>
                 </a>
 
-                <a href="index.html" class="logo logo-light">
+                <a href="{{ route('index') }}" class="logo logo-light">
                     <span class="logo-sm">
-                        <img src="/images/logo-light.svg" alt="" height="22">
+                        <img src="/images/favicon.png" alt="" height="30">
                     </span>
                     <span class="logo-lg">
-                        <img src="/images/logo-light.png" alt="" height="19">
+                        <img src="/images/logo-light.png" alt="" height="30">
                     </span>
                 </a>
             </div>
